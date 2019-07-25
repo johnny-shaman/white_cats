93a7442d39b716107179c99a0a66c801d27bad93
johnny-shaman <johnny.shaman.saxophonist@gmail.com>
2019-07-25 16:51:41 +0900

    modified:   cat.js

--- cat.js
+++ cat.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
   let _ = function (p) {
-    return Object.create(_.Types[p.constructor.name] || _.prototype, {
+    return Object.create(_.Types[p.constructor.name] || _.Types.Object, {
       _: {
         get () {
           return p;
@@ -11,83 +11,117 @@
   }
   Object.defineProperties(_.prototype, {
     lift: {
+      configurable: true,
       value (...f) {
         return _(this).endo(...f);
       }
     },
     endo: {
+      configurable: true,
       value (...f) {
         return _(this.flat(...f))
       }
     },
     flat: {
+      configurable: true,
       value (...f) {
         return this._ == null ? this : f.reduce((a, g) => g(a), this._)
       }
+    },
+    eq: {
+      configurable: true,
+      value (f) {
+        return (...g) => this.flat(f) ? this.endo(...g) : this;
+      }
+    },
+    nt: {
+      configurable: true,
+      value (f) {
+        return (...g) => this.flat(f) ? this : this.endo(...g);
+      }
+    },
+    is: {
+      configurable: true,
+      value (t) {
+        return this.eq(o => o.constructor === t);
+      }
+    },
+    of: {
+      configurable: true,
+      value (t) {
+        return this.eq(o => o instanceof t);
+      }
+    },
+    json: {
+      configurable: true,
+      get () {
+        return this.endo(JSON.stringify);
+      }
     }
   });
-  Object.assign(_, {
-    Types: Object.create(Object.prototype, {
-      Object: {
-        value: Object.create(_.prototype, {
-          fold: {
-            value (f) {
-              return this.endo(Object.entries, (a) => a.reduce((p, [k, v]) => f(p, k, v)))
-            }
-          },
-          keys: {
-            get () {
-              return this.endo(Object.keys);
-            }
-          },
-          vals: {
-            get () {
-              return this.endo(Object.values);
-            }
-          },
-          entries: {
-            get () {
-              return this.endo(Object.entries);
-            }
-          },
-          define: {
-            value (o) {
-              return this.endo(p => Object.defineProperties(p, o));
-            }
-          },
-          get: {
-            value (...k) {
-              return this.lift(t => k.reduce((p, c) => p.set));
-            }
-          },
-          set: {
-            value (...o) {
-              return this.endo(p => Object.assign(p, ...o));
-            }
-          },
-          call: {
-            value (k, ...v) {
-              return this.endo(o => o[k](...v));
-            }
-          },
-          been: {
-            get () {
-              return new Proxy(this._, {
-                get (t, k) {
-                  switch (k) {
-                    case 'to': return this;
-                    case '_': return this._;
-                    default: return (...v) => t[k].constructor === Function && t[k](...v) , t;
-                  }
-                }
-              })
+  Object.assign(_, {Types: {Object: Object.create(_.prototype)}});
+  Object.defineProperties(_.Types.Object, {
+    fold: {
+      configurable: true,
+      value (f) {
+        return this.endo(Object.entries, (a) => a.reduce((p, [k, v]) => f(p, k, v)))
+      }
+    },
+    keys: {
+      configurable: true,
+      get () {
+        return this.endo(Object.keys);
+      }
+    },
+    vals: {
+      configurable: true,
+      get () {
+        return this.endo(Object.values);
+      }
+    },
+    entries: {
+      configurable: true,
+      get () {
+        return this.endo(Object.entries);
+      }
+    },
+    define: {
+      configurable: true,
+      value (o) {
+        return this.endo(p => Object.defineProperties(p, o));
+      }
+    },
+    get: {
+      configurable: true,
+      value (...k) {
+        return this.flat(o => k.reduce((p, c) => p.set({[c]: o[c]}), _({})));
+      }
+    },
+    put: {
+      configurable: true,
+      value (...o) {
+        return this.endo(p => Object.assign(p, ...o));
+      }
+    },
+    call: {
+      configurable: true,
+      value (k, ...v) {
+        return this.endo(o => o[k](...v));
+      }
+    },
+    been: {
+      configurable: true,
+      get () {
+        return new Proxy(this._, {
+          get (t, k) {
+            switch (k) {
+              case 'to': return this;
+              case '_': return this._;
+              default: return (...v) => t[k].constructor === Function && t[k](...v) , this.been;
             }
           }
-        })
-      },
-      Array: {
-
+        });
       }
-    })
+    }
   });
 })();
