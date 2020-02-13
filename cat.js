f1efbef8d906044d83843ac4229f2bf37b6b3ea6
johnny-shaman <johnny.shaman.saxophonist@gmail.com>
2020-02-13 11:16:46 +0900

    modified:   cat.js

--- cat.js
+++ cat.js
@@ -33,7 +33,7 @@
   };
 
   Object.assign(_, {
-    WhiteCats: '0.1.26',
+    WhiteCats: '0.1.27',
     '#': (
       ['Object', 'String', '*', 'Promise']
       .reduce((p, c) => Object.assign(p, {[c]: Object.create(_.prototype)}), {})
